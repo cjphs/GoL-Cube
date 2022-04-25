0047f72e25ab396bd88fb20b99a3504bce90360a
Christopher Schiffmann <72268429+christophersch@users.noreply.github.com>
2022-04-25 22:17:21 +0200

    Fixes

--- golcube.js
+++ golcube.js
@@ -1,7 +1,39 @@
 // Grid size (grid will be n^2)
 const n = 16;
 
+const DEBUG = false;
+
+let selected_face = 0;
+let selected_x = 0;
+let selected_y = 0;
+let prev_selected_x = 0;
+let prev_selected_y = 0;
+let prev_selected_face = 0;
+
+let paused = true;
+
+let frame = 0; 
+let update_speed = document.getElementById("update_speed").value;  // how many frames between grid updates?
+let prev_mx = Infinity;
+let prev_my = Infinity;
+let mouse_down = false;
+
+let rot_vx = 0;
+let rot_vy = 0;
+
+const canvas_ids = ["canvas_east","canvas_west","canvas_up","canvas_down","canvas_north","canvas_south"];
+let canvas_textures = [null,null,null,null,null,null];
+
+const grid_colour_back = "navy"
+const grid_colour_back_lines = "blue"
+const grid_colour_alive = "lime"
+
+const TAU = Math.PI*2;
+
 class Grid {
+    grid = [];
+    redraw_required = true;
+
     constructor() {
         this.grid = Array2D.build(n,n,0);
 
@@ -48,6 +80,7 @@ class Grid {
 
     // Core update procedure for the grid instance
     update() {
+
         let next_state = Array2D.build(n,n,0);
 
         let neighbor_matrix = this.buildNeighborhoodArray();
@@ -63,8 +96,11 @@ class Grid {
                 
                 // Dead cells with 3 neighbors become alive
                 if (this.grid[r][c] == 0)
-                    if (alive_cells == 3)
+                    if (alive_cells == 3) 
                         next_state[r][c] = 1;
+
+                if (this.grid[r][c] != next_state[r][c])
+                    this.redraw_required = true;
             }
         }
 
@@ -140,6 +176,7 @@ function updateGrids() {
     }
 }
 
+
 /////////////////////
 // INITIALIZATIONS //
 /////////////////////
@@ -151,9 +188,7 @@ let east    = new Grid();
 let up      = new Grid();
 let down    = new Grid();
 
-const faces = [north,east,south,west,up,down];
-const canvas_ids = ["canvas_north","canvas_east","canvas_south","canvas_west","canvas_up","canvas_down"];
-let canvas_textures = [null,null,null,null,null,null];
+const faces = [east,west,up,down,north,south];
 
 
 // Define each face's neighbors (rotations calculated by hand, there's probably a nicer way to do this)
@@ -191,36 +226,12 @@ down.addNeighbor(1, north,      0 );
 down.addNeighbor(2, east,       C );
 down.addNeighbor(3, south,      F );
 
-// glider
-north.setCell(3,7,1);
-north.setCell(4,7,1);
-north.setCell(5,7,1);
-north.setCell(5,6,1);
-north.setCell(4,5,1);
-
-// glider
-south.setCell(3,7,1);
-south.setCell(4,7,1);
-south.setCell(5,7,1);
-south.setCell(5,6,1);
-south.setCell(4,5,1);
-
-east.setCell(7,5,1);
-east.setCell(7,4,1);
-east.setCell(7,6,1);
-
-west.setCell(3,3,1);
-west.setCell(3,4,1);
-west.setCell(4,4,1);
-west.setCell(4,6,1);
-west.setCell(5,4,1);
-
 // test the cube sides in 2D
 // TODO: don't draw sides that aren't visible
+//       current solution: lazy drawing (each grid has a redraw flag)
 
+let size = 400;
 function drawGrids2D() {
-    let size = 400;
-
     for(var i = 0; i < faces.length; i++) {
         const canvas = document.getElementById(canvas_ids[i]);
         const ctx = canvas.getContext('2d');
@@ -230,29 +241,43 @@ function drawGrids2D() {
             ctx.canvas.height = size;
         }
 
-        ctx.fillStyle = 'white'
-        ctx.globalAlpha = 0.3;
-        ctx.fillRect(0, 0, size, size);
-        ctx.globalAlpha = 1;
+        ctx
 
-        ctx.fillStyle = 'dodgerblue';
+        if (faces[i].redraw_required) {
+            if (DEBUG)
+                console.log('redraw face ' + canvas_ids[i]);
 
-        for(var y = 0; y < n; y++) {
-            for(var x = 0; x < n; x++) {
-                if (faces[i].getGrid()[y][x] == 1) {
-                    ctx.fillRect(x / n * size, y / n * size, size/n, size/n);
-                } else {
-                    ctx.strokeRect(x / n * size, y / n * size, size/n, size/n);
+            ctx.fillStyle = grid_colour_back;
+            ctx.fillRect(0, 0, size, size);
+            ctx.lineWidth = 1;
+            
+            for(var y = 0; y < n; y++) {
+                for(var x = 0; x < n; x++) {
+                    ctx.fillStyle = grid_colour_alive;
+                    ctx.strokeStyle = grid_colour_back_lines;
+
+                    if (faces[i].getGrid()[y][x] == 1) {
+                        ctx.fillRect(x / n * size, y / n * size, size/n, size/n);
+                    } else {
+                        ctx.strokeRect(x / n * size, y / n * size, size/n, size/n);
+                    }
                 }
             }
+            faces[i].redraw_required = false;
         }
 
+        // Selection highlighter
+        if (canvas_ids[i] == canvas_ids[selected_face]) {
+            ctx.strokeStyle = 'red';
+            ctx.lineWidth = 3;
+            ctx.strokeRect(selected_x / n * size, selected_y / n * size, size/n, size/n);
+        }
+
+        // Initialize the canvas texture
         if (canvas_textures[i] == null) {
             canvas_textures[i] = new THREE.CanvasTexture(ctx.canvas);
+            canvas_textures[i].magFilter = THREE.NearestFilter;
         }
-
-
-        ctx.canvas.hidden = true;
     }
 }
 
@@ -268,52 +293,56 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 const renderer = new THREE.WebGLRenderer({antialias: false});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+renderer.domElement.style.position = 'absolute';
+renderer.domElement.style.left = 0;
+renderer.domElement.style.top = 0;
 
 const materials = [
-    new THREE.MeshBasicMaterial({map: canvas_textures[1]}), // East
-    new THREE.MeshBasicMaterial({map: canvas_textures[3]}), // West
-    new THREE.MeshBasicMaterial({map: canvas_textures[4]}), // Up
-    new THREE.MeshBasicMaterial({map: canvas_textures[5]}), // Down
-    new THREE.MeshBasicMaterial({map: canvas_textures[0]}), // North
-    new THREE.MeshBasicMaterial({map: canvas_textures[2]})  // South
+    new THREE.MeshStandardMaterial({map: canvas_textures[0]}), // East
+    new THREE.MeshStandardMaterial({map: canvas_textures[1]}), // West
+    new THREE.MeshStandardMaterial({map: canvas_textures[2]}), // Up
+    new THREE.MeshStandardMaterial({map: canvas_textures[3]}), // Down
+    new THREE.MeshStandardMaterial({map: canvas_textures[4]}), // North
+    new THREE.MeshStandardMaterial({map: canvas_textures[5]})  // South
 ];
 
 const geometry = new THREE.BoxGeometry();
 const cube = new THREE.Mesh( geometry, materials );
 scene.add( cube );
 
+
+const amblight = new THREE.AmbientLight(0x0000ff,.08);
+scene.add(amblight);
+
+const light = new THREE.DirectionalLight( 0xffffff, .9 );
+light.position.set(camera.position.x, camera.position.y, camera.position.z + 4);
+light.target = cube;
+scene.add(light);
+
 cube.rotation.y = Math.PI/4;
 cube.rotation.x = Math.PI/6;
 
 camera.position.z = 2;
 
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
+
+
 //////////////////
 // UPDATE STUFF //
 //////////////////
 
-let frame = 0; 
-let update_speed = 15;  // how many frames between grid updates?
-let prev_mx = Infinity;
-let prev_my = Infinity;
-let mouse_down = false;
-
-const TAU = 2*Math.PI;
-
-
-let rot_vx = 0;
-let rot_vy = 0;
-
-var update = function(){
+function update() {
     frame++;
 
-    if (frame % update_speed == 0) {
+    if (!paused && frame % update_speed == 0) {
         updateGrids();
     }
 
-    update_speed = document.getElementById('update_speed').value;
+    update_speed = 60 - document.getElementById('update_speed').value;
 
-    
-    cube.rotation.x += rot_vx;
+    if (cube.rotation.x + rot_vx < Math.PI/2 && cube.rotation.x + rot_vx > -Math.PI/2)
+        cube.rotation.x += rot_vx;
     cube.rotation.y += rot_vy;
 
     rot_vx = lerp(rot_vx, 0, .2);
@@ -322,36 +351,84 @@ var update = function(){
     drawGrids2D();
     canvas_textures.forEach((canvas) => canvas.needsUpdate = true);
 
+	// update the picking ray with the camera and pointer position
+	raycaster.setFromCamera( pointer, camera );
+
+	// calculate objects intersecting the picking ray
+	const intersects = raycaster.intersectObjects( scene.children );
+
+    if (intersects[0] != null) {
+        prev_selected_face = selected_face;
+        prev_selected_x = selected_x;
+        prev_selected_y = selected_y;
+
+	    selected_face = Math.floor(intersects[0].faceIndex/2);
+        selected_x = Math.floor(intersects[0].uv.x * n);
+        selected_y = (n-1) - Math.floor(intersects[0].uv.y * n);
+
+        if (prev_selected_x != selected_x || prev_selected_y != selected_y) 
+            faces[selected_face].redraw_required = true;
+
+        if (prev_selected_face != selected_face && prev_selected_face != null)
+            faces[prev_selected_face].redraw_required = true;
+    } else {
+        if (selected_face != null)
+            faces[selected_face].redraw_required = true;
+        selected_face = null;
+        selected_x = 0;
+        selected_y = 0;
+    }
+
     renderer.render(scene, camera);
     requestAnimationFrame(update);  
 }
 update();
 
 
+///////////////
+// LISTENERS //
+///////////////
+
 window.addEventListener('mousedown', e => {
     mouse_down = true;
+    if (selected_face != null) {
+        faces[selected_face].setCell(selected_x,selected_y,1);
+        faces[selected_face].redraw_required = true;
+        drawGrids2D();
+    }
 });
 
 window.addEventListener('mouseup', e => {
     mouse_down = false;
 });
 
-
 window.addEventListener('mousemove', e => {
-    if (prev_mx != Infinity && mouse_down) {
-        let dx = e.x - prev_mx;
-        let dy = e.y - prev_my;
+    if (selected_face == null) {
+        if (prev_mx != Infinity && mouse_down && e.y > 60) {
+            let dx = e.x - prev_mx;
+            let dy = e.y - prev_my;
 
-        rot_vx = dy * .005;
-        rot_vy = dx * .005;
+            rot_vx = dy * .005;
+            rot_vy = dx * .005;
 
-        cube.rotation.x = (cube.rotation.x) % (TAU);
-        cube.rotation.y = (cube.rotation.y) % (TAU);
-        
+            cube.rotation.x = (cube.rotation.x) % (TAU);
+            cube.rotation.y = (cube.rotation.y) % (TAU);
+            
+        }
+    } else if (mouse_down) {
+        faces[selected_face].setCell(selected_x,selected_y,1);
+        faces[selected_face].redraw_required = true;
+        drawGrids2D();
     }
+        
 
     prev_mx = e.x;
     prev_my = e.y;
+});
+
+window.addEventListener( 'pointermove', e => {
+    pointer.x = ( e.x/ window.innerWidth ) * 2 - 1;
+    pointer.y = - ((e.y)/ window.innerHeight ) * 2 + 1;
 });
 
 // https://stackoverflow.com/questions/20290402/three-js-resizing-canvas
@@ -362,6 +439,63 @@ function onWindowResize(){
     renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
+document.getElementById('playbutton').addEventListener("click", e => {
+    pause_unpause();
+});
+
+document.getElementById('stepbutton').addEventListener("click", e => {
+    frame = 0;
+    paused = false;
+    updateGrids();
+    drawGrids2D();
+    paused = true;
+});
+
+document.getElementById('clearbutton').addEventListener("click", e => {
+    faces.forEach(face => {
+
+        face.grid = Array2D.fill(face.grid, 0);
+        face.redraw_required = true;
+    })
+});
+
+window.addEventListener('keydown', (event) => {
+    const keyName = event.key;
+  
+    if (keyName === ' ') {
+      pause_unpause();
+      return;
+    }
+  });
+
+window.addEventListener('wheel', e => {
+    cube.position.z = clamp(cube.position.z - .0025 * e.deltaY, -.5, 1);
+});
+
+
+////////////////////
+// MISC FUNCTIONS //
+////////////////////
+
 function lerp (start, end, step){
     return (1-step)*start+step*end
+}
+
+function clamp (x, a, b){
+    return Math.max(a, Math.min(x, b));
+}
+
+function pause_unpause() {
+    (paused ? unpause() : pause());
+}
+
+function pause() {
+    paused = true;
+    document.getElementById('playbutton').innerHTML = "Play";
+}
+
+function unpause() {
+    paused = false;
+    document.getElementById('playbutton').innerHTML = "Pause";
+    frame = update_speed - 1;
 }
--- js-modules/Array2D.js
+++ js-modules/Array2D.js
@@ -7,6 +7,7 @@
   // Baseline setup
   // ==============
 
+
   // Establish the root object, `window` in the browser, or `exports` on the server.
   var root = this;
 
